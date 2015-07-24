e45d80bb03a43aca5bb0c05bd51e38f92d97609b
Sauchye <sauchyer@gmail.com>
2015-07-24 20:54:02 +0800

    Initial Commit

---
+++ SYADDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SYADDemo
+//
+//  Created by Sauchye on 7/24/15.
+//  Copyright (c) 2015 sauchye.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
